fd2199355d1424c3fe19fa9d977990030245ab4c
Javy <Javy@Javy-PC>
2015-04-19 13:19:52 -0500

    By Javy

---
+++ operacionesBasicas/src/operacionesbasicas/OperacionesBasicas.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacionesbasicas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Javy
+ */
+public class OperacionesBasicas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        operaciones op = new operaciones();
+   
+        float a,b;
+        a = Float.valueOf(JOptionPane.showInputDialog(null,"Numero 1"));
+        b = Float.valueOf(JOptionPane.showInputDialog(null,"Numero 2"));
+        
+        op.suma(a,b);
+    }
+}
---
+++ operacionesBasicas/src/operacionesbasicas/operaciones.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacionesbasicas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Javy
+ */
+public class operaciones {
+    
+    float resultado;
+    void suma(float numero1, float numero2){
+        resultado = numero1 + numero2;
+        System.out.println("Suma de dos numeros----> " + resultado);
+        JOptionPane.showMessageDialog(null, "Resultado--->"+resultado);
+    }
+            
+}
