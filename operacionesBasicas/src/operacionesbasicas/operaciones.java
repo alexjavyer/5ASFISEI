96b74fad9143cb4e51496ebe85baabb74164d896
alexjavyer <alex.javyer@gmail.com>
2015-04-20 12:43:53 -0500

    Update operaciones.java

--- operacionesBasicas/src/operacionesbasicas/operaciones.java
+++ operacionesBasicas/src/operacionesbasicas/operaciones.java
@@ -25,6 +25,11 @@ public class operaciones {
         JOptionPane.showMessageDialog(null, "Resultado--->"+resultado);
     }
     
+    void resta1(float numero1, float numero2){
+        resultado = numero1 - numero2;
+        System.out.println("Suma de dos numeros----> " + resultado);
+        JOptionPane.showMessageDialog(null, "Resultado--->"+resultado);
+    }
      void multiplicacion(float numero1, float numero2){
         resultado = numero1 * numero2;
         System.out.println("Suma de dos numeros----> " + resultado);
