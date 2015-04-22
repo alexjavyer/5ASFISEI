9fa1bc05c8b9f5b3504e56c07faf8e2650e11fe9
Javy <Javy@Javy-PC>
2015-04-22 16:34:59 -0500

    Operaciones Modificadas

--- operacionesBasicas/src/operacionesbasicas/operaciones.java
+++ operacionesBasicas/src/operacionesbasicas/operaciones.java
@@ -18,19 +18,7 @@ public class operaciones {
         System.out.println("Suma de dos numeros----> " + resultado);
         JOptionPane.showMessageDialog(null, "Resultado--->"+resultado);
     }
-    
-     void resta(float numero1, float numero2){
-        resultado = numero1 - numero2;
-        System.out.println("Suma de dos numeros----> " + resultado);
-        JOptionPane.showMessageDialog(null, "Resultado--->"+resultado);
-    }
-    
-    void resta1(float numero1, float numero2){
-        resultado = numero1 - numero2;
-        System.out.println("Suma de dos numeros----> " + resultado);
-        JOptionPane.showMessageDialog(null, "Resultado--->"+resultado);
-    }
-     void multiplicacion(float numero1, float numero2){
+    void multiplicacion(float numero1, float numero2){
         resultado = numero1 * numero2;
         System.out.println("Suma de dos numeros----> " + resultado);
         JOptionPane.showMessageDialog(null, "Resultado--->"+resultado);
